147f948604149b2dd11323cf6aa4f36486b09ad7
Felipe Gomez <35458612+A01378889@users.noreply.github.com>
2018-04-12 08:36:24 -0500

    Add files via upload

---
+++ Botones/Menu.py
@@ -0,0 +1,61 @@
+# encoding: UTF-8
+# Autor: Roberto Martínez Román
+# Muestra cómo utilizar pygame para escribir programas que dibujan en la pantalla
+
+import pygame
+
+# Dimensiones de la pantalla
+ANCHO = 800
+ALTO = 600
+# Colores
+BLANCO = (255, 255, 255)  # R,G,B en el rango [0,255]
+VERDE_BANDERA = (0, 122, 0)
+ROJO = (255, 0, 0)
+AZUL = (0, 0, 255)
+
+
+# Estructura básica de un programa que usa pygame para dibujar
+def dibujar():
+    # Inicializa el motor de pygame
+    pygame.init()
+    ventana = pygame.display.set_mode((ANCHO, ALTO))  # Crea la ventana de dibujo
+    reloj = pygame.time.Clock()  # Para limitar los fps
+    termina = False  # Bandera para saber si termina la ejecución
+
+    #Imagenes
+    imgFondo = pygame.image.load("fondoMenu.png")
+    imgBtnJugar = pygame.image.load("button_jugar.png")
+    imgBtnAcercaDe = pygame.image.load("button_acerca-de.png")
+    spriteBtnJugar = pygame.sprite.Sprite()
+    spriteBtnJugar.image = imgBtnJugar
+    spriteBtnJugar.rect = imgBtnJugar.get_rect()
+    spriteBtnJugar.rect.left = 290
+    spriteBtnJugar.rect.top = 300
+
+
+    while not termina:  # Ciclo principal
+        # Procesa los eventos que recibe el programa
+        for evento in pygame.event.get():
+            if evento.type == pygame.QUIT:  # El usuario hizo click en el botón de salir
+                termina = True
+
+        # Borrar pantalla
+        ventana.fill(BLANCO)
+
+        # Dibujar, aquí haces todos los trazos que requieras
+        # Normalmente llamas a otra función y le pasas -ventana- como parámetro, por ejemplo, dibujarLineas(ventana)
+        ventana.blit(imgFondo, (0,0))
+        ventana.blit(spriteBtnJugar.image, spriteBtnJugar.rect)
+
+        pygame.display.flip()  # Actualiza trazos
+        reloj.tick(40)  # 40 fps
+
+    # Después del ciclo principal
+    pygame.quit()  # termina pygame
+
+
+def main():
+    dibujar()
+
+
+main()
